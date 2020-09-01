abf971c4d3bf3591e4285d82ed8a7524d43b548b
zhangning02 <zhangning02@qutoutiao.net>
2020-09-01 20:51:59 +0800

    Create Projet

---
+++ app/src/main/java/com/jifen/dandan/ringtone/RingToneManager.java
@@ -0,0 +1,252 @@
+package com.jifen.dandan.ringtone;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import com.jifen.dandan.common.utils.AppUtils;
+import com.jifen.dandan.common.utils.LogUtils;
+import com.jifen.dandan.ringtone.permission.PermissionUtils;
+
+import java.io.File;
+
+
+public class RingToneManager {
+
+    private RingToneManager() {
+    }
+
+    private Context mContext;
+    private RingtoneManager mRingtoneManager;
+    private Ringtone mRingtone;
+
+    private static class RingToneManagerHolder {
+        private static final RingToneManager INSTANCE = new RingToneManager();
+    }
+
+    public static RingToneManager getInstance() {
+        return RingToneManagerHolder.INSTANCE;
+    }
+
+    public void init(Context context) {
+        this.mContext = context;
+//        mRingtoneManager = new RingtoneManager(context.getApplicationContext());
+//        mRingtoneManager.setStopPreviousRingtone(true);
+    }
+
+
+    /**
+     * 设置新的铃声
+     *
+     * @param path
+     */
+    public void setRingtoneImpl(String path) {
+        File ringtoneFile = new File(path);
+        if (ringtoneFile == null) {
+            LogUtils.e("file is null");
+            return;
+        }
+        ContentValues content = new ContentValues();
+        content.put(MediaStore.MediaColumns.DATA, ringtoneFile.getAbsolutePath());
+        content.put(MediaStore.MediaColumns.TITLE, ringtoneFile.getName());
+        content.put(MediaStore.MediaColumns.SIZE, ringtoneFile.length());
+        content.put(MediaStore.MediaColumns.MIME_TYPE, "audio/*");
+        //  content.put(MediaStore.Audio.Media.ARTIST, "Madonna");
+        //content.put(MediaStore.Audio.Media.DURATION, 230);
+        content.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+        content.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);
+        content.put(MediaStore.Audio.Media.IS_ALARM, false);
+        content.put(MediaStore.Audio.Media.IS_MUSIC, false);
+        // 获取文件是external还是internal的uri路径
+        Uri uri = MediaStore.Audio.Media.getContentUriForPath(ringtoneFile.getAbsolutePath());
+        // 铃声通过contentvaues插入到数据库
+        final Uri newUri = mContext.getContentResolver().insert(uri, content);
+        RingtoneManager.setActualDefaultRingtoneUri(AppUtils.getAppContext(), RingtoneManager.TYPE_RINGTONE, newUri);
+    }
+
+    public void setRingtoneImpl2(String path) {
+        File chosenFile = new File(path);
+        if (chosenFile == null) {
+            LogUtils.e("file is null");
+            return;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DATA, chosenFile.getAbsolutePath());
+        values.put(MediaStore.MediaColumns.TITLE, chosenFile.getName());
+        values.put(MediaStore.MediaColumns.SIZE, chosenFile.length());
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");
+        values.put(MediaStore.Audio.AudioColumns.IS_RINGTONE, true);
+        values.put(MediaStore.Audio.AudioColumns.IS_NOTIFICATION, false);
+        values.put(MediaStore.Audio.AudioColumns.IS_ALARM, false);
+        values.put(MediaStore.Audio.AudioColumns.IS_MUSIC, false);
+
+        Uri uri = MediaStore.Audio.Media.getContentUriForPath(chosenFile.getAbsolutePath());
+        mContext.getContentResolver().delete(uri, MediaStore.MediaColumns.DATA + "=\"" + chosenFile.getAbsolutePath() + "\"", null);
+
+        Uri newUri = mContext.getContentResolver().insert(uri, values);
+
+        try {
+            RingtoneManager.setActualDefaultRingtoneUri(
+                    mContext,
+                    RingtoneManager.TYPE_RINGTONE,
+                    newUri
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Settings.System.putString(mContext.getContentResolver(), "ringtone2", newUri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Settings.System.putString(mContext.getContentResolver(), "ringtone_sim2", newUri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Settings.System.putString(mContext.getContentResolver(), "ringtone_2", newUri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void getSettinsProviderStrin() {
+        String[] rings = new String[]{"ringtone2", "ringtone_sim2","ringtone_2","ringtone"};
+
+        for (int i = 0; i < rings.length; i++) {
+            Uri uri = getUri(rings[i]);
+            log("uri = " + uri);
+//            LogUtils.e(" RingToneManager uri = " + uri);
+        }
+    }
+
+    private void log(String msg) {
+        LogUtils.e("RingToneManager", msg);
+    }
+    private Uri getUri(String c) {
+        if (TextUtils.isEmpty(c)) {
+            return null;
+        }
+        String string = Settings.System.getString(mContext.getContentResolver(), c);
+        log("RingToneManager c = " + c + "get Uri string = " + string);
+        if (TextUtils.isEmpty(string)) {
+            return null;
+        }
+        return Uri.parse(string);
+    }
+
+    /**
+     * 删除铃声
+     *
+     * @param context
+     * @param deleteUri
+     * @param deleteFile
+     */
+    public void deleteCustomRingtone(Context context, Uri deleteUri, File deleteFile) {
+        ContentValues cv = new ContentValues();
+        cv.put(MediaStore.Audio.Media.IS_RINGTONE, false);
+        cv.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);
+        cv.put(MediaStore.Audio.Media.IS_ALARM, false);
+        cv.put(MediaStore.Audio.Media.IS_MUSIC, true);
+        // 更新当前铃声的数据，放弃作为铃声的状态
+        context.getContentResolver().delete(deleteUri, MediaStore.MediaColumns.DATA + "=?",
+                new String[]{deleteFile.getAbsolutePath()});
+    }
+
+    public void stopRing() {
+//        if (mRingtoneManager != null) {
+//            mRingtoneManager.setStopPreviousRingtone(true);
+//            mRingtoneManager.stopPreviousRingtone();
+//        }
+//        if (mRingtone != null) {
+//            mRingtone.stop();
+//        }
+    }
+
+    public void playRing() {
+//        mRingtone = RingtoneManager.getRingtone(mContext, Settings.System.DEFAULT_RINGTONE_URI);
+//
+//        mRingtone.setLooping(true);
+//        mRingtone.play();
+    }
+
+
+
+    private int setPermission(Activity mActivity) {
+        int num = 0;
+        try {
+            StringBuilder des = new StringBuilder();
+            if (!PermissionUtils.checkFloatWindowPermission(mActivity)) {
+                des.append(num2string(num) + "设置来电视频悬浮框");
+                des.append("\n");
+                num++;
+            }
+
+            if (!PermissionUtils.hasSettingPermisson(mActivity)) {
+                des.append(num2string(num) + "修改铃声设置权限");
+                des.append("\n");
+                num++;
+            }
+
+            if (!PermissionUtils.isNotificationListenersEnabled(mActivity)) {
+                des.append(num2string(num) + "设置来电通知权限");
+                des.append("\n");
+                num++;
+            }
+
+            if (!PermissionUtils.checkStartForeground(mActivity)) {
+                des.append(num2string(num) + "开启后台弹出页面");
+                des.append("\n");
+                num++;
+            }
+
+            if (!PermissionUtils.checkLockedScreen(mActivity)) {
+                des.append(num2string(num) + "开启来电显示");
+                des.append("\n");
+                num++;
+            }
+
+            if (num > 0) {
+                if (mActivity != null && !mActivity.isDestroyed() && !mActivity.isFinishing()) {
+                    RingPermissionDialog ringPermissionDialog = RingPermissionDialog.newInstance(mActivity.getPageName(),des.toString());
+                    ringPermissionDialog.show(mActivity.getSupportFragmentManager(), "ring_permission_dialog");
+                }
+            }
+        } catch (Exception e) {
+
+        }
+
+        return num;
+    }
+
+
+    private String num2string(int num){
+        if (num == 0) {
+            return "① ";
+        } else if (num == 1) {
+            return "② ";
+        } else if (num == 2) {
+            return "③ ";
+        } else if (num == 3) {
+            return "④ ";
+        } else if (num == 4) {
+            return "⑤ ";
+        } else if (num == 5) {
+            return "⑥ ";
+        }
+        return "";
+    }
+
+}
---
+++ app/src/main/java/com/jifen/dandan/ringtone/permission/HuaweiUtils.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2016 Facishare Technology Co., Ltd. All Rights Reserved.
+ */
+package com.jifen.dandan.ringtone.permission;
+
+import android.annotation.TargetApi;
+import android.app.AppOpsManager;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.jifen.dandan.common.utils.LogUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class HuaweiUtils {
+    private static final String TAG = "HuaweiUtils";
+
+    /**
+     * 检测 Huawei 悬浮窗权限
+     */
+    public static boolean checkFloatWindowPermission(Context context) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= 19) {
+            return checkOp(context, 24); //OP_SYSTEM_ALERT_WINDOW = 24;
+        }
+        return true;
+    }
+
+    public static void gotoOverlySetPage(Context context) {
+        try {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//   ComponentName comp = new ComponentName("com.huawei.systemmanager","com.huawei.permissionmanager.ui.MainActivity");//华为权限管理
+//   ComponentName comp = new ComponentName("com.huawei.systemmanager",
+//      "com.huawei.permissionmanager.ui.SingleAppActivity");//华为权限管理，跳转到指定app的权限管理位置需要华为接口权限，未解决
+            ComponentName comp = new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.addviewmonitor.AddViewMonitorActivity");//悬浮窗管理页面
+            intent.setComponent(comp);
+            if (RomUtils.getEmuiVersion() == 3.1) {
+                //emui 3.1 的适配
+                context.startActivity(intent);
+            } else {
+                //emui 3.0 的适配
+                comp = new ComponentName("com.huawei.systemmanager", "com.huawei.notificationmanager.ui.NotificationManagmentActivity");//悬浮窗管理页面
+                intent.setComponent(comp);
+                context.startActivity(intent);
+            }
+        } catch (SecurityException e) {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//   ComponentName comp = new ComponentName("com.huawei.systemmanager","com.huawei.permissionmanager.ui.MainActivity");//华为权限管理
+            ComponentName comp = new ComponentName("com.huawei.systemmanager",
+                    "com.huawei.permissionmanager.ui.MainActivity");//华为权限管理，跳转到本app的权限管理页面,这个需要华为接口权限，未解决
+//      ComponentName comp = new ComponentName("com.huawei.systemmanager","com.huawei.systemmanager.addviewmonitor.AddViewMonitorActivity");//悬浮窗管理页面
+            intent.setComponent(comp);
+            context.startActivity(intent);
+            LogUtils.e(TAG, Log.getStackTraceString(e));
+        } catch (ActivityNotFoundException e) {
+            /**
+             * 手机管家版本较低 HUAWEI SC-UL10
+             */
+//   Toast.makeText(MainActivity.this, "act找不到", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName comp = new ComponentName("com.Android.settings", "com.android.settings.permission.TabItem");//权限管理页面 android4.4
+//   ComponentName comp = new ComponentName("com.android.settings","com.android.settings.permission.single_app_activity");//此处可跳转到指定app对应的权限管理页面，但是需要相关权限，未解决
+            intent.setComponent(comp);
+            context.startActivity(intent);
+            LogUtils.e(TAG, Log.getStackTraceString(e));
+        } catch (Exception e) {
+            //抛出异常时提示信息
+            Toast.makeText(context, "进入设置页面失败，请手动设置", Toast.LENGTH_LONG).show();
+            LogUtils.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * 去华为默认设置页面
+     */
+    public static void applyPermission(Context context) {
+        try {
+            Intent intent = new Intent(context.getPackageName());
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName comp = new ComponentName("com.huawei.systemmanager", "com.huawei.permissionmanager.ui.MainActivity");
+            intent.setComponent(comp);
+            context.startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(context, "跳转失败,青岛", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            PermissionUtils.gotoAppDetailSettingIntent(context);
+        }
+    }
+
+    //com.android.packageinstaller/.permission.ui.ManagePermissionsActivity
+    public static void toPermisstionSetting(Context context) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction("android.intent.action.MANAGE_APP_PERMISSIONS");
+        ComponentName comp = new ComponentName("com.android.packageinstaller", "com.android.packageinstaller.permission.ui.ManagePermissionsActivity");
+        intent.setComponent(comp);
+        context.startActivity(intent);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static boolean checkOp(Context context, int op) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= 19) {
+            AppOpsManager manager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            try {
+                Class clazz = AppOpsManager.class;
+                Method method = clazz.getDeclaredMethod("checkOp", int.class, int.class, String.class);
+                return AppOpsManager.MODE_ALLOWED == (int) method.invoke(manager, op, Binder.getCallingUid(), context.getPackageName());
+            } catch (Exception e) {
+                LogUtils.e(TAG, Log.getStackTraceString(e));
+            }
+        } else {
+            LogUtils.e(TAG, "Below API 19 cannot invoke!");
+        }
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static boolean canBackgroundStart(Context context) {
+        AppOpsManager ops = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        try {
+            int op = 10021; // >= 23
+            // ops.checkOpNoThrow(op, uid, packageName)
+            Method method = ops.getClass().getMethod("checkOpNoThrow", new Class[]{
+                            int.class, int.class, String.class
+                    }
+            );
+
+            Integer result = (Integer) method.invoke(ops, op, Binder.getCallingUid(), context.getPackageName());
+            return result == AppOpsManager.MODE_ALLOWED;
+        } catch (Exception e) {
+            Log.e(TAG, "not support", e);
+        }
+        return false;
+    }
+
+    /**
+     * 检测后台弹出页面
+     *
+     * @param context
+     * @return
+     */
+    public static boolean checkStartForeground(Context context) {
+//        final int version = Build.VERSION.SDK_INT;
+//        if (version >= 19) {
+//            boolean ret = canBackgroundStart(context);
+//            LogUtils.e("huawei checkStartForeground =  " + ret + " op 76 = " + checkOp(context, 76));
+//
+//            return ret; //OP_START_FOREGROUND = 76;
+//        }
+        return true;
+
+    }
+
+    /**
+     * 检测锁屏显示权限
+     * @param context
+     * @return
+     */
+    public static boolean checkLockedScreen(Context context) {
+        return true;
+    }
+
+    public static boolean isAl10() {
+        return RomUtils.checkIsHuaweiRom() && (Build.MODEL.contains("AL10"));
+    }
+
+}
+
+
